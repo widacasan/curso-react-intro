c14d647c3df3e1bd0065538bb89a541fd6912410
William David <98925022+widacasan@users.noreply.github.com>
2023-12-01 11:51:41 -0500

    Tips para naming y abstracción de componentes React

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
 import React from "react";
-import { TodoCounter } from "../TodoCounter";
-import { TodoSearch } from "../TodoSearch";
-import { TodoList } from "../TodoList";
-import { TodoItem } from "../TodoItem";
-import { CreateTodoButton } from "../CreateTodoButton";
 import { useLocalStorage } from "./useLocalStorage";
+import { AppUI } from "./AppUI";
 
 function App() {
   const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
@@ -31,22 +27,15 @@ function App() {
     saveTodos(newTodos);
   };
   return (
-    <>
-      <TodoCounter completed={completedTodos} total={totalTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-      <CreateTodoButton />
-    </>
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 export default App;
