7bfafe9145b1c582f96b3827ddc2196b6ae7f932
William David <98925022+widacasan@users.noreply.github.com>
2023-12-04 11:26:07 -0500

    Loading skeletons

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -2,6 +2,9 @@ import { TodoCounter } from "../TodoCounter";
 import { TodoSearch } from "../TodoSearch";
 import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
+import { TodosLoading } from "../TodosLoading";
+import { TodosError } from "../TodosError";
+import { EmptyTodos } from "../EmptyTodos";
 import { CreateTodoButton } from "../CreateTodoButton";
 
 function AppUI({
@@ -20,9 +23,9 @@ function AppUI({
       <TodoCounter completed={completedTodos} total={totalTodos} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
-        {loading && <p>Estamos cargando...</p>}
-        {error && <p>Desesperate, hubo un error!!</p>}
-        {!loading && searchedTodos.length === 0 && <p>Crea tu primer TODO!!</p>}
+        {loading && <TodosLoading />}
+        {error && <TodosError />}
+        {!loading && searchedTodos.length === 0 && <EmptyTodos />}
         {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
---
+++ src/TodosLoading/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TodosLoading.css";
+
+function TodosLoading() {
+  return <p className="loader"></p>;
+}
+
+export { TodosLoading };
